70f9c16480fe369c8e52cae837535624fc422058
MaNnu81 <mannuspamm@gmail.com>
2025-03-17 17:30:19 +0100

    card

--- src/detail.js
+++ src/detail.js
@@ -21,7 +21,7 @@ function render(monster) {
     const monsterDetail = document.getElementById("monster-detail");
     monsterDetail.innerHTML = "";
     const monsterName = document.createElement("span");
-    let node = document.createTextNode(monster.name);
+    let node = document.createTextNode('Nome: ' + monster.name);
     monsterName.appendChild(node);
     const monsterImg = document.createElement("img");
     if (monster.image === undefined) {
@@ -29,24 +29,22 @@ function render(monster) {
     } else {
         monsterImg.src = BASE_URL + monster.image;
     }
-    monsterImg.width = "200";
+    
+    monsterImg.classList.add('imported-img'); 
+
     const monsterType = document.createElement("span");
-    node = document.createTextNode(monster.type);
+    node = document.createTextNode('Type: ' + monster.type);
     monsterType.appendChild(node);
     const monsterHP = document.createElement("span");
-    node = document.createTextNode(monster.hit_points);
+    node = document.createTextNode('Hit Point: ' + monster.hit_points);
     monsterHP.appendChild(node);
     const monsterXp = document.createElement("span");
-    node = document.createTextNode(monster.xp);
+    node = document.createTextNode('XP: ' + monster.xp);
     monsterXp.appendChild(node);
-    const monsterUrl = document.createElement("a");
-    if (monster.url === undefined) {
-        node = document.createTextNode("non disponibile");
-    } else {
-        node = document.createTextNode("link al json");
-        monsterUrl.href=BASE_URL + monster.url;
-    }
-    monsterUrl.target = "_blank";
-    monsterUrl.appendChild(node);
-    monsterDetail.append(monsterImg, monsterName, monsterType, monsterHP, monsterXp, monsterUrl);
+  
+    
+    monsterDetail.append(monsterImg, monsterName, monsterType, monsterHP, monsterXp);
 }
+
+
+'Autore: ' + author.name
